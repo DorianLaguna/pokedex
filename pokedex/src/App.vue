c5510bc8fa28896b50ccae4360e2ba1e3ca81c35
DorianLaguna <richardlgcs@gmail.com>
2025-01-17 18:50:35 -0600

    crear heder

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,17 +1,41 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'
+
 </script>
 
 <template>
-  <Header>
-    
-  </Header>
 
-  <RouterView />
+  <Header class="header"/>
+
+  <RouterView class="container" />
+
+  <footer>
+
+  </footer>
+
 </template>
 
 <style scoped>
+  .header{
+    width: 100%;
+    padding: 20px;
+    background-color: var(--secundary-color);
+  }
+  .container{
+    max-width: 900px;
+    margin: 0 auto;
+    background-color: var(--primary-color);
+  }
+  main {
+    margin: 0 auto;  
+    min-height: 10vh;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+  }
+  footer {
+    margin-top: auto;
+  }
 
 </style>
--- pokedex/src/components/Header.vue
+++ pokedex/src/components/Header.vue
@@ -1,15 +1,31 @@
-
 <script setup>
 
 </script>
 
 <template>
-    <div>
-        Hola
-
-    </div>
+    <header class="header">
+        <div class="titulo">
+            POKEDEX
+        </div>
+        <nav>
+            <a href="My profile">My profile</a>
+        </nav>
+    </header>
 </template>
 
 <style scoped>
-
+    .titulo{
+        font-size: var(--font-medium);
+        color: var(--primary-color);
+        font-weight: 800;
+    }
+    .header{
+        background-color: var(--secondary-color);
+    }
+    @media (min-width: 768px){
+        .header{
+            display: flex;
+            justify-content: space-between;
+        }
+    }
 </style>
